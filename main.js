bb6d9d222ebcc598f84a3014133a7341c6eeba9e
Abdullohte <abdulloht210@gmail.com>
2025-01-07 21:49:42 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,13 @@
-let ball = +prompt("O'zingizning ballingizni kiriting:");
 
-if (ball > 50) {
-    alert("Tabriklaymiz! Ballingiz yetarli!");
-    let uylanganmisiz = confirm("Uylanganmisiz?");
-    if (uylanganmisiz) {
-        alert("Uzur, uylanganlarni o‘qishga olmaymiz.");
-    } else {
-        alert("Tabriklaymiz! Siz o‘qishga kirdiz.");
-    }
-} else {
-    alert("Afsus, sizning natijangiz 50 dan past.");
-    let uylanganmi = confirm("Uylanganmisiz?");
-    if (!uylanganmi) {
-        alert("Tabriklaymiz! Siz o‘qishga kirdiz.Chunki siz hali uylanmagansiz");
-    } else {
-        alert("uzur uylanganlarni o‘qishga kirmadiz.");
-    }
-}
+
+
+let num1 = Math.floor(Math.random() * 100) + 1; 
+let num2 = Math.floor(Math.random() * 100) + 1; 
+let product = num1 * num2;
+let dividedResult = product / 2;
+
+alert(`Числа: ${num1} и ${num2}`);
+alert(`Произведение: ${product}`);
+alert(`Результат деления в двое :${dividedResult}`);
+
+
